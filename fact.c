397dcf24254c2bfe4b5a373b06c3b9d70cb56ec9
Loudus123 <loudusraj@gmail.com>
2016-09-18 11:02:31 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    unsigned long factorial = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+
+  if (n < 0)
+        printf("Error!);
+
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;     
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+
+    return 0;
+}
